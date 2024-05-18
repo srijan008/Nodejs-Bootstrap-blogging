e005eaafcd5fba3340306444c2b277fd7c21b3a0
srijan008 <srijankumar008@gmail.com>
2024-05-18 10:23:58 +0530

    Add all file from vscode

---
+++ app.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const express = require('express');
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+const mongoose = require("mongoose");
+const cookiePaser = require('cookie-parser');
+const { checkForAuthenticationCookie } = require('./middleware/authentication');
+const path = require("path");
+
+const Blog = require('./models/blog')
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(e => console.log("MongoDB Connected"));
+
+    
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.static(path.resolve('./public')))
+app.use(express.urlencoded({extended: false}));
+app.use(cookiePaser());
+
+app.use(checkForAuthenticationCookie("token"));
+
+app.get("/", async (req, res) => {    
+    const allBlogs = await Blog.find({})
+    res.render("home",{
+        user: req.user,
+        blogs: allBlogs,
+    });
+});
+
+app.use("/users", userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
